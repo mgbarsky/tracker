62f65c0f22ddda1863c0c03107e935241c59c6aa
MGBarsky <mgbarsky@gmail.com>
2025-02-02 21:57:44 -0500

    It is working on server

--- src/logic/App.jsx
+++ src/logic/App.jsx
@@ -1,30 +1,46 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-import Schedule from '../screens/Schedule';
-import Tasks from '../screens/Tasks';
-import Metrics from '../screens/Metrics';
-import { Metric } from '../objects/metric.js';
-import { Task } from '../objects/task.js';
+import Schedule from "../screens/Schedule";
+import Tasks from "../screens/Tasks";
+import Metrics from "../screens/Metrics";
+import { Metric } from "../objects/metric.js";
+import { Task } from "../objects/task.js";
 
-import { Routes, Route } from 'react-router-dom';
-
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [tasks, setTasks] = useState(Task.defaultTasks)
-  const [metrics, setMetrics] = useState(Metric.defaultMetrics)
-  const [records, setRecords] = useState ([])
-  
-  return (
-    <>   
-    <Schedule tasks={tasks} metrics={metrics} records={records} setRecords={setRecords}/>
-     {/* <Routes>
-        <Route path="./" element={<Schedule tasks={tasks} metrics={metrics} records={records} setRecords={setRecords}/> }></Route>
-        <Route path="./tasks" element={<Tasks tasks={tasks} setTasks={setTasks}/>}></Route>
-        <Route path="./metrics" element={<Metrics metrics={metrics} setMetrics={setMetrics}/>}></Route>
-      </Routes> */}
-    </>
-  )
+    const [tasks, setTasks] = useState(Task.defaultTasks);
+    const [metrics, setMetrics] = useState(Metric.defaultMetrics);
+    const [records, setRecords] = useState([]);
+
+    return (
+        <>
+            <Routes basename="/myapp">
+                <Route
+                    path="/"
+                    element={
+                        <Schedule
+                            tasks={tasks}
+                            metrics={metrics}
+                            records={records}
+                            setRecords={setRecords}
+                        />
+                    }
+                ></Route>
+                <Route
+                    path="/tasks"
+                    element={<Tasks tasks={tasks} setTasks={setTasks} />}
+                ></Route>
+                <Route
+                    path="/metrics"
+                    element={
+                        <Metrics metrics={metrics} setMetrics={setMetrics} />
+                    }
+                ></Route>
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
