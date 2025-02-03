1f22672de036d371cd1bbec76ac26d02f5e55dce
Aung Wai Yan Hein <aungwaiyanhein@Aungs-MacBook-Pro.local>
2025-02-03 15:02:33 -0700

    change basename of the browser router depending on whether it is in production or dev

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 
+const PRODUCTION_SUB_DIRECTORY = "/myapp3";
+const basename = import.meta.env.PROD ? PRODUCTION_SUB_DIRECTORY : "/";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename="/myapp3">
+        <BrowserRouter basename={basename}>
             <App />
         </BrowserRouter>
     </React.StrictMode>
